d72ab410bca530d8cd9677dad2d60fde79fb2862
soji-opa <azeezolasoji@gmail.com>
2020-07-30 20:57:19 +0200

    Improved my understanding of classess and inheritances in python

--- src/classes.py
+++ src/classes.py
@@ -21,19 +21,19 @@
 
 """ We can make it cleaner by utilizing constructors """
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def move(self):
-        print("move")
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#     def move(self):
+#         print("move")
     
-    def draw(self):
-        print("draw")
+#     def draw(self):
+#         print("draw")
 
-point = Point(10, 20)
-# point.x = 11
-print(point.x)
+# point = Point(10, 20)
+# # point.x = 11
+# print(point.x)
 
 """ Exercise """
 
@@ -51,18 +51,45 @@ print(point.x)
 
 """ Inheritance basics """
 
-class Mammals:
-    def walk(self):
-        print("walk")
+# class Mammals:
+#     def walk(self):
+#         print("walk")
 
-class Dogs(Mammals):
-    def bark(self):
-        print("bark bark")
+# class Dogs(Mammals):
+#     def bark(self):
+#         print("bark bark")
 
-class Cats(Mammals):
-    pass
+# class Cats(Mammals):
+#     pass
 
-dog1= Dogs()
+# dog1= Dogs()
 
-dog1.walk()
-dog1.bark()
+# dog1.walk()
+# dog1.bark()
+
+class Parent:
+    def __init__ (self, name, location, work):
+        self.name = name
+        self.location = location
+        self.work = work
+
+    def job(self):
+        return (f"{self.name} is a {self.work}")
+
+
+mom = Parent ("Brittany", "CPT", "developer")
+print(mom.job())
+# Inheritance
+
+class Child(Parent):
+    def __init__(self, major):
+        self.major = major
+    
+    def study(self):
+        return(f"{self.name} is a {self.major}")
+
+
+# cindy = Child ("Comp sci")
+# print(cindy.study())
+# print(cindy.job())
+
